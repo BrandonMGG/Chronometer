489a91104efda84b5df6b06b34659c2a836d162b
traznar <ggbrandonm@gmail.com>
2023-08-20 23:13:15 -0600

    PR-01 Display Working

--- Taller1.sv
+++ Taller1.sv
@@ -1,10 +1,16 @@
 module Taller1(input logic	clk,rst_n,
 						input logic switch, 
-						output logic[9:0] leds);
+						output logic[6:0] seg0,seg1,seg2,seg3,seg4,seg5, output logic[2:0] leds);
 platform plat
 (
 	.clk_clk(clk),
-	.leds_external_connection_export(leds),
+	.pio_0_external_connection_export(leds),
+	.leds_external_connection_export(seg0),
+	.seven_seg1_external_connection_export(seg1),
+	.seven_seg2_external_connection_export(seg2),
+	.seven_seg3_external_connection_export(seg3),
+	.seven_seg4_external_connection_export(seg4),
+	.seven_seg5_external_connection_export(seg5),
 	.switch_external_connection_export(switch),
 	.reset_reset_n(rst_n)
 );
---
+++ software/test/hello_world_small.c
@@ -0,0 +1,52 @@
+#include "sys/alt_irq.h"
+#include "altera_avalon_pio_regs.h"
+#include "altera_avalon_timer_regs.h"
+#include "system.h"
+
+static unsigned leds = 0;
+static int digit=0;
+static int display=0;
+
+static void timer_isr(void *context)
+{
+(void) context;
+	leds = leds << 1 | (IORD_ALTERA_AVALON_PIO_DATA(SWITCH_BASE) & 1);
+	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, leds);
+	if (digit == 0) {
+	    display = 64;
+	} else if (digit == 1) {
+	    display = 121;
+	} else if (digit == 2) {
+	    display = 36;
+	} else if (digit == 3) {
+	    display = 48;
+	} else if (digit == 4) {
+	    display = 25;
+	} else if (digit == 5) {
+	    display = 18;
+	} else if (digit == 6) {
+	    display = 2;
+	} else if (digit == 7) {
+	    display = 120;
+	} else if (digit == 8) {
+	    display = 0;
+	} else if (digit == 9) {
+	    display = 24;
+	    digit=0;
+	}
+	digit+=1;
+	IOWR_ALTERA_AVALON_PIO_DATA(SEVEN_SEG1_BASE, display);
+	IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE, 0);
+}
+int main(){
+	alt_ic_isr_register(
+		TIMER_0_IRQ_INTERRUPT_CONTROLLER_ID,
+		TIMER_0_IRQ,
+		timer_isr,
+		NULL,
+		NULL);
+	IOWR_ALTERA_AVALON_TIMER_CONTROL(
+		TIMER_0_BASE,
+		ALTERA_AVALON_TIMER_CONTROL_ITO_MSK | ALTERA_AVALON_TIMER_CONTROL_CONT_MSK);
+	while (1);
+}
